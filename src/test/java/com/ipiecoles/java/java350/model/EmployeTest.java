d6855de31420cb7873dc00c4059204303f5a97e7
Pierre-Julien VILLOUD <pjvilloud@protonmail.com>
2021-05-04 11:02:43 +0200

    Add UT scenario on getPrimeAnnuelle

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -94,6 +94,7 @@ public class EmployeTest {
     @CsvSource({
             "0,,'M12345',1.0,1700.0", //Manager à plein temps sans ancienneté
             "0,,'T12345',1.0,1000.0", //Technicien à plein temps sans ancienneté
+            "0,1,'T12345',1.0,1000.0", //Technicien à plein temps sans ancienneté avec performance de base
             //"0,,'M12345',0.5,850.0", //Manager à mi-temps sans ancienneté
             //"5,,'M12345',1.0,2200.0", //Manager à plein temps avec 5 années d'ancienneté
             "0,3,'T12345',1.0,3300.0", //Technicien à plein temps sans ancienneté avec performance 3
